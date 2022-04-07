161c4db33234a1f61970dfa0ae45a4bb84ed5944
Manuel.von.Au <vonau.manuel@gmail.com>
2022-04-07 21:55:00 +0200

    add,RoundOneFragment.java+FullScreenImageFragment, full screen image works

--- app/src/main/java/com/example/pubquizremote/RoundOneFragment.java
+++ app/src/main/java/com/example/pubquizremote/RoundOneFragment.java
@@ -20,23 +20,24 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
 
-public class RoundOneFragment extends Fragment {
+
+public class RoundOneFragment extends Fragment implements View.OnClickListener {
 
 
     private FragmentRoundOneBinding binding;
     FirebaseAuth auth;
     String uid;
     private String link;
+    HashMap<String, String> imageUrls = new HashMap<String, String>();
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-
         binding = FragmentRoundOneBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
         return root;
 
 
@@ -55,47 +56,87 @@ public class RoundOneFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Log.w("Debug_A", "RoundOneFragment: "+String.valueOf(requireActivity()));
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance("https://pub-quiz-remote-default-rtdb.europe-west1.firebasedatabase.app");
-        auth = FirebaseAuth.getInstance();
-        String uid = auth.getCurrentUser().getUid();
-        DatabaseReference ref_image = database.getReference("question_answer_pairs").child("1").child("QustionAnswerPair").child("picture");
+        String[] questions = {"11","12","13","14","15","16"};
+        for (String s: questions) {
 
-        ref_image.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            FirebaseDatabase database = FirebaseDatabase.getInstance("https://pub-quiz-remote-default-rtdb.europe-west1.firebasedatabase.app");
+            auth = FirebaseAuth.getInstance();
+            String uid = auth.getCurrentUser().getUid();
+            DatabaseReference ref_round_one = database.getReference("question_answer_pairs").child("1").child(s).child("picture");
+            ref_round_one.addListenerForSingleValueEvent(new ValueEventListener() {
 
-                link = dataSnapshot.getValue(String.class);
-                Picasso.get().load(link).into(binding.Round1Picture1);
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-            }
+                    link = dataSnapshot.getValue(String.class);
+                    //Log.w("Debug_A", "Link: "+link );
+
+                    switch (s) {
+                        case "11":
+                            Picasso.get().load(link).into(binding.pic11);
+                            imageUrls.put("pic11",link);
+                        case "12":
+                            Picasso.get().load(link).into(binding.pic12);
+                            imageUrls.put("pic12",link);
+                        case "13":
+                            Picasso.get().load(link).into(binding.pic13);
+                            imageUrls.put("pic13",link);
+                        case "14":
+                            Picasso.get().load(link).into(binding.pic14);
+                            imageUrls.put("pic14",link);
+                        case "15":
+                            Picasso.get().load(link).into(binding.pic15);
+                            imageUrls.put("pic15",link);
+                        case "16":
+                            Picasso.get().load(link).into(binding.pic16);
+                            imageUrls.put("pic16",link);
+                    }
+                }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // we are showing that error message in toast
+                        Log.w("Debug_A", "error: loading image", databaseError.toException());
+                    }
+                });
+            };
 
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // we are showing that error message in toast
-                Log.w("Debug_A", "error: loading image", databaseError.toException());
-            }
-        });
-
-
-
-        binding.Round1Picture1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Navigation.findNavController(getView()).navigate(R.id.action_nav_round_one_to_fullScreenImageFragment);
-
-                Bundle result = new Bundle();
-                result.putString("bundleKey", link);
-                getParentFragmentManager().setFragmentResult("requestKey", result);
-
-            }
-        });
-
-
+        binding.pic11.setOnClickListener(this);
+        binding.pic12.setOnClickListener(this);
+        binding.pic13.setOnClickListener(this);
+        binding.pic14.setOnClickListener(this);
+        binding.pic15.setOnClickListener(this);
+        binding.pic16.setOnClickListener(this);
 
     }
 
+    @Override
+    public void onClick(View view) {
+
+        Navigation.findNavController(getView()).navigate(R.id.action_nav_round_one_to_fullScreenImageFragment);
+        Bundle result = new Bundle();
+        switch (view.getId()) {
+            case R.id.pic11:
+                result.putString("bundleKey",imageUrls.get("pic11"));
+                break;
+            case R.id.pic12:
+                result.putString("bundleKey",imageUrls.get("pic12"));
+                break;
+            case R.id.pic13:
+                result.putString("bundleKey",imageUrls.get("pic13"));
+                break;
+            case R.id.pic14:
+                result.putString("bundleKey",imageUrls.get("pic14"));
+                break;
+            case R.id.pic15:
+                result.putString("bundleKey",imageUrls.get("pic15"));
+                break;
+            case R.id.pic16:
+                result.putString("bundleKey",imageUrls.get("pic16"));
+                break;
+        }
+        getParentFragmentManager().setFragmentResult("requestKey", result);
+    }
 }
