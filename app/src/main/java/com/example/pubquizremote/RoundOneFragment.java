fec4119a8961c3b581297e6614d1221e2b274a63
Manuel.von.Au <vonau.manuel@gmail.com>
2022-04-08 23:23:06 +0200

    fix,RoundOneFragment, most error fixed but loading second part of pics doesn't work yet

--- app/src/main/java/com/example/pubquizremote/RoundOneFragment.java
+++ app/src/main/java/com/example/pubquizremote/RoundOneFragment.java
@@ -66,7 +66,23 @@ public class RoundOneFragment extends Fragment implements View.OnClickListener {
         Boolean page2 = binding.radioButtonOne.isChecked();
         Log.w("Debug_A","Output: "+String.valueOf(page1));
 
-        String[] questions = {"1","2","3","4","5","6"};
+        load_images_from_db(page1,page2);
+
+        binding.pic1.setOnClickListener(this);
+        binding.pic2.setOnClickListener(this);
+        binding.pic3.setOnClickListener(this);
+        binding.pic4.setOnClickListener(this);
+        binding.pic5.setOnClickListener(this);
+        binding.pic6.setOnClickListener(this);
+        binding.radioButtonOne.setOnClickListener(this);
+        binding.radioButtonTwo.setOnClickListener(this);
+
+    }
+
+
+    void load_images_from_db(Boolean page1,Boolean page2){
+
+        String[] questions = {"1","2","3","4","5","6"}; //,"21","22","23","24","25","26"};
         for (String s: questions) {
 
             FirebaseDatabase database = FirebaseDatabase.getInstance("https://pub-quiz-remote-default-rtdb.europe-west1.firebasedatabase.app");
@@ -82,49 +98,53 @@ public class RoundOneFragment extends Fragment implements View.OnClickListener {
                     link = dataSnapshot.child("picture").getValue(String.class);
                     page = dataSnapshot.child("page").getValue().toString();
 
-                    if (page.equals("1")) {
+                    //if (page.equals("1")) {
+                    if (page1) {
+                        Log.w("Debug_A","Link1: "+link);
                         switch (s) {
                             case "1":
                                 Picasso.get().load(link).into(binding.pic1);
-                                imageUrls.put("pic11", link);
+                                imageUrls.put("pic1", link);
                             case "2":
                                 Picasso.get().load(link).into(binding.pic2);
-                                imageUrls.put("pic12", link);
+                                imageUrls.put("pic2", link);
                             case "3":
                                 Picasso.get().load(link).into(binding.pic3);
-                                imageUrls.put("pic13", link);
+                                imageUrls.put("pic3", link);
                             case "4":
                                 Picasso.get().load(link).into(binding.pic4);
-                                imageUrls.put("pic14", link);
+                                imageUrls.put("pic4", link);
                             case "5":
                                 Picasso.get().load(link).into(binding.pic5);
-                                imageUrls.put("pic15", link);
+                                imageUrls.put("pic5", link);
                             case "6":
                                 Picasso.get().load(link).into(binding.pic6);
-                                imageUrls.put("pic16", link);
+                                imageUrls.put("pic6", link);
                         }
 
-                }
-                    else if (page.equals("2")){
+                    }
+                    //else if (page.equals("2")){
+                    else if (page2){
+                        Log.w("Debug_A","Link2: "+link);
                         switch (s) {
                             case "1":
                                 Picasso.get().load(link).into(binding.pic1);
-                                imageUrls.put("pic21", link);
+                                imageUrls.put("pic1", link);
                             case "2":
                                 Picasso.get().load(link).into(binding.pic2);
-                                imageUrls.put("pic22", link);
+                                imageUrls.put("pic2", link);
                             case "3":
                                 Picasso.get().load(link).into(binding.pic3);
-                                imageUrls.put("pic23", link);
+                                imageUrls.put("pic3", link);
                             case "4":
                                 Picasso.get().load(link).into(binding.pic4);
-                                imageUrls.put("pic24", link);
+                                imageUrls.put("pic4", link);
                             case "5":
                                 Picasso.get().load(link).into(binding.pic5);
-                                imageUrls.put("pic25", link);
+                                imageUrls.put("pic5", link);
                             case "6":
                                 Picasso.get().load(link).into(binding.pic6);
-                                imageUrls.put("pic26", link);
+                                imageUrls.put("pic6", link);
                         }
                     }
                     else {
@@ -134,26 +154,15 @@ public class RoundOneFragment extends Fragment implements View.OnClickListener {
                 }
 
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        // we are showing that error message in toast
-                        Log.w("Debug_A", "error: loading image", databaseError.toException());
-                    }
-                });
-            };
-
-
-        binding.pic1.setOnClickListener(this);
-        binding.pic2.setOnClickListener(this);
-        binding.pic3.setOnClickListener(this);
-        binding.pic4.setOnClickListener(this);
-        binding.pic5.setOnClickListener(this);
-        binding.pic6.setOnClickListener(this);
-        binding.radioButtonOne.setOnClickListener(this);
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // we are showing that error message in toast
+                    Log.w("Debug_A", "error: loading image", databaseError.toException());
+                }
+            });
+        };
 
     }
-
-
 
 
 
@@ -161,47 +170,21 @@ public class RoundOneFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
 
         String tag = String.valueOf(view.getTag());
-        Log.w("Debug_A","Tag: "+String.valueOf(tag));
+        Log.w("Debug_A","Tag: "+tag);
 
         if (tag.equals("radioButtonOne")) {
             Toast.makeText(getContext(), "Page 1 clicked", Toast.LENGTH_SHORT).show();
+            load_images_from_db(true,false);
+        }
+        else if (tag.equals("radioButtonTwo")) {
+            Toast.makeText(getContext(), "Page 2 clicked", Toast.LENGTH_SHORT).show();
+            load_images_from_db(false,true);
         }
         else {
             Navigation.findNavController(getView()).navigate(R.id.action_nav_round_one_to_fullScreenImageFragment);
             Bundle result = new Bundle();
             result.putString("bundleKey",imageUrls.get(tag));
-    }
-
-
-
-        /*
-        Navigation.findNavController(getView()).navigate(R.id.action_nav_round_one_to_fullScreenImageFragment);
-        Bundle result = new Bundle();
-        switch (view.getId()) {
-            case R.id.radioButtonOne:
-                Toast.makeText(getContext(), "Page 1 clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.pic11:
-                result.putString("bundleKey",imageUrls.get("pic11"));
-                break;
-            case R.id.pic12:
-                result.putString("bundleKey",imageUrls.get("pic12"));
-                break;
-            case R.id.pic13:
-                result.putString("bundleKey",imageUrls.get("pic13"));
-                break;
-            case R.id.pic14:
-                result.putString("bundleKey",imageUrls.get("pic14"));
-                break;
-            case R.id.pic15:
-                result.putString("bundleKey",imageUrls.get("pic15"));
-                break;
-            case R.id.pic16:
-                result.putString("bundleKey",imageUrls.get("pic16"));
-                break;
+            getParentFragmentManager().setFragmentResult("requestKey", result);
         }
-        getParentFragmentManager().setFragmentResult("requestKey", result);
-
-        */
     }
 }
