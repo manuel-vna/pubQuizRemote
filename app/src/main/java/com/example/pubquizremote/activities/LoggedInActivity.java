e1c0bb9a72108675376caf1f190f6e4c70e00b9b
Manuel.von.Au <vonau.manuel@gmail.com>
2022-05-03 22:20:04 +0200

    change, mobile_navigation.xml+activity_main_draer.xml+LoggedInActivity.java, mobile navigation renewed

--- app/src/main/java/com/example/pubquizremote/activities/LoggedInActivity.java
+++ app/src/main/java/com/example/pubquizremote/activities/LoggedInActivity.java
@@ -71,7 +71,7 @@ public class LoggedInActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_round_one, R.id.nav_image_round)
+                R.id.nav_infopage, R.id.nav_round1, R.id.nav_round2, R.id.nav_round3,R.id.nav_round4)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_logged_in_home);
